22b29c24c2991823fcccc9e126a56c595da24690
cathe-zhang <zhangle_media@hotmail.com>
2019-08-21 15:55:03 +0800

    refactor: 精简page末班内容, 删除多余注释

--- templates/page.tpl.js
+++ templates/page.tpl.js
@@ -1,9 +1,9 @@
 // 页面模版
 const echoPageTemplate = (pathname) => {
   return `import Taro, { Component, Config } from '@tarojs/taro';
-import { View } from '@tarojs/components';
-// tslint:disable-next-line:no-implicit-dependencies
+import { connect } from '@tarojs/redux';
 import { ComponentClass } from 'react';
+import { View } from '@tarojs/components';
 import { IPageState, IProps, IPageOwnProps } from './${pathname}.itf'
 import './${pathname}.scss';
 
@@ -12,9 +12,10 @@ interface Index {
   // this的属性可以在这里定义
 }
 
-/**
- * 界面
- */
+@connect(({}) => {
+	return {};
+})
+
 class Index extends Component {
   config: Config = {
     navigationBarTitleText: ''
@@ -22,19 +23,10 @@ class Index extends Component {
 
   state: IPageState = {};
 
-  /**
-   * 在微信/百度/字节跳动/支付宝小程序中这一生命周期方法对应 app 的 onLaunch，在 componentWillMount 后执行
-   */
   componentDidMount() {}
 
-  /**
-   * 页面显示/切入前台时触发
-   */
   componentDidShow() {}
 
-  /**
-   * render 函数
-   */
   render() {
     return (
       <View className='${pathname}-page'>
